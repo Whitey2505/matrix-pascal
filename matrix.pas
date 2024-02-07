2b0e38e3d1d6a3549d0da5195f11a96339f26ec7
Andrew White <35773966+Whitey2505@users.noreply.github.com>
2024-02-07 21:36:27 +0000

    Create matrix.pas
    matrix falling code demo in turbo pascal 3

---
+++ matrix.pas
@@ -0,0 +1,48 @@
+rogram fallingcode(output);
+
+var
+   a,x,y,z,b : integer;
+
+procedure rand;
+             begin
+               x := Random(60)+1;
+               y := Random(20)+1;
+          end;
+
+procedure rand1;
+          begin
+               b :=random(2)+1;
+          end;
+
+procedure ones;
+          begin
+          rand;
+          for a :=1 to 9 do
+              begin
+                   rand1;
+                   gotoXY(x,y+a);
+                   if b = 1 then
+                   write('1');
+                   if b<> 1 then
+                   write('0');
+          end;
+          end;
+
+procedure zeros;
+          begin
+               rand;
+               for a := 1 to 10 do
+                   begin
+                        gotoXY(x,y+a);
+                        write(' ');
+                   end;
+          end;
+begin
+     clrscr;
+     for z:= 1 to 600 do
+         begin
+              ones;
+              zeros;
+         end;
+
+end.
